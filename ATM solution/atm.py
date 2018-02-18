784e0d87650cf01d64b6fc64aedca5de7854198f
Abooorh0 <36489410+Abooorh0@users.noreply.github.com>
2018-02-18 13:29:37 +0300

    Update atm.py

--- ATM solution/atm.py
+++ ATM solution/atm.py
@@ -1,28 +1,37 @@
 def atm(money, request):
 
-	while request > 0:
+	if request > money:
+		print "Can't gvies all this money!"
 
-		if request >= 100:
-			request -= 100
-			print "Give 100"
+	elif request <= 0:
+		print "Please inter the number?"
 		
-		elif request >= 50:
-			request -= 50
-			print "Give 50"
+	else:
+			
+		while request > 0:
 
-		elif request >= 10:
-			request -= 10
-			print "Give 10"	
+			if request >= 100:
+				request -= 100
+				print "Give 100"
+		
+			elif request >= 50:
+				request -= 50
+				print "Give 50"
 
-		elif request >= 5:
-			request -= 5
-			print "Give 5"
+			elif request >= 10:
+				request -= 10
+				print "Give 10"	
 
-		elif request >= 1:
-			request -= 1
-			print "Give 1"
+			elif request >= 5:
+				request -= 5
+				print "Give 5"
 
-	return request
+			elif request >= 1:
+				request -= 1
+				print "Give 1"
+    			
+
+		return request
 
 money = 500
-money = atm(money, 389)	
+money = atm(money, 376)	
