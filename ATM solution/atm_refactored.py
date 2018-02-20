f369643179778a7e2c20ae7ab7ac658e6fe9bd16
Abooorh <abeeroo_93@hotmail.com>
2018-02-20 22:45:54 +0300

    Functions

---
+++ ATM solution/atm_refactored.py
@@ -0,0 +1,42 @@
+money = 500
+
+def withdraw(money, request):
+    print "Current balance = " + str (money)
+    
+    if   request > money:
+        print "Can't gvies all this money!"
+
+    elif request <= 0:
+        print "Please inter the number?"
+
+    else:
+        money -= request
+        while request > 0:
+
+            if request >= 100:
+                request -= 100
+                print "Give 100"
+        
+            elif request >= 50:
+                request -= 50
+                print "Give 50"
+
+            elif request >= 10:
+                request -= 10
+                print "Give 10" 
+
+            elif request >= 5:
+                request -= 5
+                print "Give 5"
+
+            elif request >= 1:
+                request -= 1
+                print "Give 1"
+
+    return money - request
+
+
+money = withdraw(money, 376) 
+money = withdraw(money, 30)
+money = withdraw(money, 5)
+money = withdraw(money, 500)                
