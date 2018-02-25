d0cde3ff559feab9f68ffaae624fbcacf4f4fd06
Abooorh <abeeroo_93@hotmail.com>
2018-02-25 21:47:57 +0300

    Entities

---
+++ Forums/main.py
@@ -0,0 +1,8 @@
+import models
+	
+member1 = models.Member("member1", 00)
+member2 = models.Member("member2", 00)
+
+post1 = models.Post("Title1", "This is the content1.")
+post2 = models.Post("Title2", "This is the content2.")
+post3 = models.Post("Title3", "This is the content3.")
